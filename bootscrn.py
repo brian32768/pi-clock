2bda83eb016a189c7da970edd720ea36604e374a
Brian H Wilson <brian@wildsong.biz>
2019-01-06 16:08:27 -0800

    more bits and bobs

---
+++ bootscrn.py
@@ -0,0 +1,23 @@
+#!env python3
+#
+import PIL
+
+make an image and push it out to the Inky at boot time
+
+Um
+
+version number
+
+ah ah
+
+SSID
+
+IP address
+
+Anything else?
+
+Today's date?
+
+A logo
+
+
--- showtime.py
+++ showtime.py
@@ -1,4 +1,4 @@
-# env python3
+#!env python3
 
 # 1. Generate an image
 # 2. Sleep for 60 seconds
@@ -6,5 +6,37 @@
 
 Off to work on another project now. One that generates income. 
 TODO: add actual code here. :-)
+
+# We will not hit the remote weatber server every minute,
+weather_interval=15
+weather_countdown=0
+
+um how do I do a class in python again ah,,,, been doing only JavaScript lately
+class Weather:
+    
+    def _init_(self) :
+        self.currentTemperature   = 72
+        self.currentWindSpeed     = 0
+        self.currentWindDirection = 'NW'
+        return
+
+    def get(self):
+        return status
+
+w = Weather()
+
+while(1):
+    if weather_countdown <= 0:
+        w.get()
+        weather_interval = weather_countdown
+
+    # generate clock face
+
+    if pi():
+        # push image to Inky
+    else:
+        # serve image via web browser
+
+    # sleep 60 seconds
 
 
